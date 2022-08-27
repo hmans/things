af6f51c7665a2f383ff0ce607b5033bb6319621a
Hendrik Mans <hendrik@mans.de>
2022-08-27 12:44:53 +0200

    Export these guys

--- packages/hmans-things/src/index.ts
+++ packages/hmans-things/src/index.ts
@@ -1,1 +1,7 @@
-export const foo = "bar"
+export * from "@hmans/r3f-animate"
+export * from "@hmans/r3f-lifecycle"
+export * from "@hmans/r3f-shared-resource"
+export * from "@hmans/use-const"
+export * from "@hmans/use-list"
+export * from "@hmans/use-rerender"
+export * from "@hmans/use-version"
